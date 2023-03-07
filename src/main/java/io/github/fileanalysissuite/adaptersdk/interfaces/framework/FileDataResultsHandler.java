f4eab025e6b4cb37a1f317a7166659dd8bfc2b03
Krzysztof Ploch <kris@ploch.dev>
2023-03-07 10:13:34 +0000

    Revert "US627237: Removed the temporary dummy implementation (#11)"
    This reverts commit aec17bced6e9c0b0637a4c198357bf92fc0d5f7e.

--- src/main/java/io/github/fileanalysissuite/adaptersdk/interfaces/framework/FileDataResultsHandler.java
+++ src/main/java/io/github/fileanalysissuite/adaptersdk/interfaces/framework/FileDataResultsHandler.java
@@ -43,5 +43,8 @@ public interface FileDataResultsHandler extends FailureRegistration
      * @param delay the length of time to delay
      * @param cancellationToken the cancellation token
      */
-    void retryAfter(String fileId, Duration delay, CancellationToken cancellationToken);
+    default void retryAfter(String fileId, Duration delay, CancellationToken cancellationToken)
+    {
+        throw new UnsupportedOperationException("Not implemented yet - please wait for US627237 to be completed.");
+    }
 }
